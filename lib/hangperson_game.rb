efbe0fda171fe09b981ad298c066f0744908e428
Cary Schwartzstein <cary.schwartzstein@gmail.com>
2017-09-08 20:08:46 +0000

    working hangman

--- lib/hangperson_game.rb
+++ lib/hangperson_game.rb
@@ -7,9 +7,49 @@ class HangpersonGame
 
   # def initialize()
   # end
-  
+  attr_accessor :word, :guesses, :wrong_guesses
+
   def initialize(word)
     @word = word
+    @guesses = ''
+    @wrong_guesses = ''
+  end
+  
+  def guess(letter)
+    if !(letter =~ /[A-Za-z]/)
+      raise ArgumentError
+    elsif (@guesses + @wrong_guesses).include?(letter.upcase) || 
+      (@guesses + @wrong_guesses).include?(letter.downcase)
+      return false
+    end
+    if @word.include?(letter)
+      @guesses += letter
+    else
+      @wrong_guesses += letter
+    end
+    return true
+  end
+  
+  def word_with_guesses
+    to_return = ''
+    @word.each_char do |letter|
+      if @guesses.include?(letter)
+        to_return += letter
+      else
+        to_return += '-'
+      end
+    end
+    to_return
+  end
+  
+  def check_win_or_lose
+    if self.word_with_guesses == @word
+      return :win
+    elsif @wrong_guesses.length >= 7
+      return :lose
+    else
+      return :play
+    end
   end
 
   # You can test it by running $ bundle exec irb -I. -r app.rb
