5dc9bc129710dde82de0e1c177739e0a173a327f
Cary Schwartzstein <cary.schwartzstein@gmail.com>
2017-09-08 23:29:10 +0000

    up to step 5

--- app.rb
+++ app.rb
@@ -40,6 +40,14 @@ class HangpersonApp < Sinatra::Base
   post '/guess' do
     letter = params[:guess].to_s[0]
     ### YOUR CODE HERE ###
+    begin
+      valid = @game.guess(letter)
+      rescue ArgumentError
+        flash[:message] = "Invalid guess."
+    end
+    if !valid
+        flash[:message] = "Invalid guess."
+    end
     redirect '/show'
   end
   
